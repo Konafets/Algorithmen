cfd97ff56fd03e41d72b9943e0eb8eb940b90a86
Stefano Kowalke <blueduck@gmx.net>
2011-11-23 14:52:57 +0100

    Change the usage of the sorter into a generic mySorter variable
    Don't build everytime a new Variable for the special sort algorithm. Instead use a generic on.

--- src/sorter/Sort.java
+++ src/sorter/Sort.java
@@ -49,20 +49,20 @@ public class Sort
     /**
      * Here we call the various sort algoritm by dependency injection
      * 
-     * @param int randomNumber
+     * @param int size
      * @param Sorter s
      * @return String A HTML table row with some output of the called algoritm
      */
-    private static String sort(int randomNumber, Sorter s) 
+    private static String sort(int size, Sorter s) 
     {
         String result;
         setSorter(s);
-        int randomNumberArray[] = getRandomIntArray(randomNumber);   
+        int randomNumberArray[] = getRandomIntArray(size);   
         
         start = new Date().getTime();
         sorter.sort(randomNumberArray);
         runningTime = new Date().getTime() - start;
-        result = "<tr><td>" + sorter.getSorterName() + "</td><td>"  + randomNumber + "</td><td>" + runningTime + "</td></tr>";
+        result = "<tr><td>" + sorter.getSorterName() + "</td><td>"  + size + "</td><td>" + runningTime + "</td></tr>";
                 
         return result;
     };
@@ -75,26 +75,27 @@ public class Sort
         writer.generateHTML('h');
         writer.writeLn("<table style=\"border=1px solid black;\">");
         writer.writeLn("<tr><th>Sortierverfahren</th><th>Problemgröße n</th><th>Zeit</th></tr>");
-        QuickSorterYT qSorterYT = new QuickSorterYT();
-        writer.writeLn(sort(10, qSorterYT));
-        writer.writeLn(sort(1000, qSorterYT));
-        writer.writeLn(sort(10000, qSorterYT));
-        writer.writeLn(sort(100000, qSorterYT));
-		
-        InsertionSorter iSorter = new InsertionSorter();
-        writer.writeLn(sort(1000, iSorter));
-        writer.writeLn(sort(10000, iSorter));
-        writer.writeLn(sort(100000, iSorter));
-		
-        QuickSorter qSorter = new QuickSorter();
-        writer.writeLn(sort(1000, qSorter));
-        writer.writeLn(sort(10000, qSorter));
-        writer.writeLn(sort(100000, qSorter));
         
-        MergeSorter mSorter = new MergeSorter();
-        writer.writeLn(sort(1000, mSorter));
-        writer.writeLn(sort(10000, mSorter));
-        writer.writeLn(sort(100000, mSorter));
+        Sorter mySorter = new QuickSorterYT(); 
+        writer.writeLn(sort(10, mySorter));
+        writer.writeLn(sort(1000, mySorter));
+        writer.writeLn(sort(10000, mySorter));
+        writer.writeLn(sort(100000, mySorter));
+		
+        mySorter = new InsertionSorter();
+        writer.writeLn(sort(1000, mySorter));
+        writer.writeLn(sort(10000, mySorter));
+        writer.writeLn(sort(100000, mySorter));
+		
+        mySorter = new QuickSorter();
+        writer.writeLn(sort(1000, mySorter));
+        writer.writeLn(sort(10000, mySorter));
+        writer.writeLn(sort(100000, mySorter));
+        
+        mySorter = new MergeSorter();
+        writer.writeLn(sort(1000, mySorter));
+        writer.writeLn(sort(10000, mySorter));
+        writer.writeLn(sort(100000, mySorter));
         writer.writeLn("</table>");
         writer.generateHTML('f');
     }
